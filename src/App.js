64121cb6ad42dbdbb4a0ea2da50febaaa1a2cf08
Mourad Aouinat <mourad.eyes@gmail.com>
2020-02-06 04:06:44 +0100

    Add Layout components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Content from "./components/layout/Content";
+import Header from "./components/layout/Header";
 
-const App = () => <div>Hello world</div>;
+const App = () => (
+  <div>
+    <Header />
+    <Content />
+  </div>
+);
 
 export default App;
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaPizzaSlice } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist Clone" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>
+              <FaPizzaSlice />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
